6f24d008915f815a2998fb0ddfd4d2ce8f63ed94
EmilioRivFa <emiliorivfa@gmail.com>
2025-02-13 10:22:35 -0600

    Este es el js del curso extra basico

---
+++ assignments.js
@@ -0,0 +1,19 @@
+let country = 'mexico';
+let continent = 'America';
+let population = 660000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+
+lenguaje = 'español';
+const country = 'mexico';
+const continent = 'America';
+const isIsland = false;
+isIsland = true;
+console.log(typeof isIsland); //typeof significa que va a leer el tipo de dato
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof lenguaje);
