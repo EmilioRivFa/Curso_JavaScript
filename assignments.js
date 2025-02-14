69bfebf5992d812651c5e7a39a9cc974e3f3dcce
EmilioRivFa <emiliorivfa@gmail.com>
2025-02-14 10:33:43 -0600

    Cadenas

--- assignments.js
+++ assignments.js
@@ -9,11 +9,27 @@ console.log(population);
 
 
 lenguaje = 'español';
-const country = 'mexico';
-const continent = 'America';
-const isIsland = false;
+//const country = 'mexico';
+//const continent = 'America';
+//const isIsland = false;
 isIsland = true;
 console.log(typeof isIsland); //typeof significa que va a leer el tipo de dato
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof lenguaje);
+
+
+console.log(population /2);
+console.log(++ population);
+console.log(population > 6000000)
+console.log(population<33000000)
+
+let description =
+country +
+' esta en ' +
+continent + 
+' y tiene ' + 
+population + 
+' de personas hablando ' +
+lenguaje;
+console.log(description)
