f7a5ffade44f9de4efe9505973bfcc90f2f24d19
EmilioRivFa <emiliorivfa@gmail.com>
2025-02-17 09:34:56 -0600

    Conversion de tipos y coereción

--- assignments.js
+++ assignments.js
@@ -45,3 +45,10 @@ if(population > 33000000){
 }else{
     (console.log(`${country} su poblacion es ${33000000 - population} millones de avitantes`));
 }
+
+//Conversion de tipos y coerción
+console.log('9' - '5'); // -> 4 ?
+console.log('19' - '13' + '17'); // -> 617?
+console.log('19' - '13' + 17); // -> 23 ?
+console.log('123' < 57); // -> False?
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143?
