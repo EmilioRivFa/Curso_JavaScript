1a507f4dd235ba787239c78b3dc236a163ce65df
EmilioRivFa <emiliorivfa@gmail.com>
2025-02-20 11:55:09 -0600

    Tipos de Datos

--- javascriptFundamentals/scrip.js
+++ javascriptFundamentals/scrip.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40+ 8 + 23 -10);
 console.log('Jonas');
@@ -10,3 +11,24 @@ let job1 = "Programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob, myCurrentJob);
+
+*/
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'jonas');
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun)
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 2004;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
