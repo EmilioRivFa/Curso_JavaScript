405f2bddc52cb5dc22a674bf77bb7ff8d0f80459
EmilioRivFa <emiliorivfa@gmail.com>
2025-03-21 10:19:26 -0600

    Es el inicio del curso

---
+++ javascriptFundamentals/scrip.js
@@ -0,0 +1,12 @@
+let js = 'amazing';
+console.log(40+ 8 + 23 -10);
+console.log('Jonas');
+console.log(23);
+
+let myFirstJob = "Progranner";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob, myCurrentJob);
