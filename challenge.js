d0becac5611757572bf40e1fcb4a35be01ce6fe0
EmilioRivFa <emiliorivfa@gmail.com>
2025-04-02 08:08:29 -0600

    Challenge

---
+++ challenge.js
@@ -0,0 +1,38 @@
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log('Su inidec de masa moscular es ' + BMIMark, BMIJohn);
+console.log(BMIMark,BMIJohn,markHigherBMI);
+
+
+
+
+
+const firstName= 'Emilio';
+const job = 'teacher';
+const birthYear = 2004;
+const year=2025;
+
+const Emilio = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old '+ job + '!';
+console.log(Emilio) 
+
+const jonasNew = `I'm ${firstName}`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple\n\
+lines');
+
+console.log(`String with
+multiple
+lines`);
+    
+    
